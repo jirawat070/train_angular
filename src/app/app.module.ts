7420136bf251f3acc2f6c5e8545c8eff47eb54c2
jirawat070 <jirawat_pao@homail.co.th>
2020-01-17 16:36:36 +0700

    import ReactiveFormsModule,HttpClientModule,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './share/header/header.component';
 import { PlayerComponent } from './player/player.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { AddComponent } from './player/add/add.component';
+import { EditComponent } from './player/edit/edit.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    PlayerComponent
+    PlayerComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule //ใช้สำหรับเก็ตค่าของฟอร์ม --form control,Group
   ],
   providers: [],
   bootstrap: [AppComponent]
