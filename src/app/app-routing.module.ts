4c48d53101e5340fa0b1c753f385ac0c4c2989e2
jirawat070 <jirawat_pao@homail.co.th>
2020-01-17 16:34:35 +0700

    create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './player/player.component';
+import { AddComponent } from './player/add/add.component';
+import { EditComponent } from './player/edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',component: HomeComponent},
+  {path : 'player',component: PlayerComponent},
+  {path:'player/add',component:AddComponent},
+  {path:'player/edit/:id',component:EditComponent},
+  {path:'**',component:HomeComponent}
+ 
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
