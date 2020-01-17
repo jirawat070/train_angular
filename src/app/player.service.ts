ea7d395f2f8d41fd7a6d51a540f9d6e3fe1e9b4d
jirawat070 <jirawat_pao@homail.co.th>
2020-01-17 16:37:15 +0700

    create get,add api player

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,19 @@ export class PlayerService {
  
   loadPlayer(){
     return this.http.get('http://localhost:8080/api/player/list');
+  }
+  addPlayer(player:any){
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+   
+    console.log("add data" + JSON.stringify(player) );
+    return this.http.post('http://localhost:8080/api/player/create',player , {responseType: 'text'})
+  
+  }
+
+  getPlayer(id:number){
+    console.log("id" + id);
+    
+    return this.http.get(`http://localhost:8080/api/player/`+id);
+
   }
 }
