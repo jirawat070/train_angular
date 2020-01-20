1f14d7776e3355f5b8fedf254d385fa32d6285df
jirawat070 <jirawat_pao@homail.co.th>
2020-01-20 16:03:01 +0700

    edit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static('dist\my-second-angular'));
+
 
 app.listen(process.env.PORT ||8080);
