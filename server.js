497f9aea7ff94500ac284134b84c5ede23468b79
jirawat070 <jirawat_pao@homail.co.th>
2020-01-20 16:17:12 +0700

    test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-
 app.use(express.static(__dirname + '/dist/my-second-angular'));
-
-
-app.listen(process.env.PORT ||8080);
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'dist/my-second-angular', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
